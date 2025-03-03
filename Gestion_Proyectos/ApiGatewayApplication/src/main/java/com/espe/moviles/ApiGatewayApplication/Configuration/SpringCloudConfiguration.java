b5c09d74b308c939bd6afe17f00e6dabc600fbde
Angel Castillo <51182768+Agcastillo2@users.noreply.github.com>
2025-03-03 16:41:40 -0500

    eureka

--- Gestion_Proyectos/ApiGatewayApplication/src/main/java/com/espe/moviles/ApiGatewayApplication/Configuration/SpringCloudConfiguration.java
+++ Gestion_Proyectos/ApiGatewayApplication/src/main/java/com/espe/moviles/ApiGatewayApplication/Configuration/SpringCloudConfiguration.java
@@ -10,20 +10,11 @@ public class SpringCloudConfiguration {
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("bckmovilesCard", r -> r.path("/api/cards/**")
+                .route("rol", r -> r.path("/api/roles/**")
                         .uri("http://localhost:8001")
                 )
-                .route("bckmovilesNotification", r -> r.path("/api/notifications/**")
+                .route("usuario", r -> r.path("/api/usuarios/**")
                         .uri("http://localhost:8002")
-                )
-                .route("bckmovilesPayment", r -> r.path("/api/payments/**")
-                        .uri("http://localhost:8003")
-                )
-                .route("bckmovilesTransaction", r -> r.path("/api/transactions/**")
-                        .uri("http://localhost:8004")
-                )
-                .route("bckmovilesUser", r -> r.path("/api/users/**")
-                        .uri("http://localhost:8005")
                 )
                 .build();
     }
